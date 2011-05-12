4ec1fb1a77d4e4d09cbfbc00c73664e12df75b98
Clinton R. Nixon <crnixon@gmail.com>
2011-05-11 21:15:53 -0400

    Revert format changes
    Upgrading rspec fixed all my problems

--- app/controllers/api/links_controller.rb
+++ app/controllers/api/links_controller.rb
@@ -1,23 +1,24 @@
 class Api::LinksController < Api::BaseController
   def create
-    if params[:website_url].blank?    
+    if params[:website_url].blank?
       website_url = params[:link][:website_url]
     else
       website_url = params[:website_url]
     end
-    @link = Link.find_or_create_by_website_url( website_url )
-    @link.ip_address = request.remote_ip if @link.new_record?      
 
-    if @link.save        
+    @link = Link.find_or_create_by_website_url( website_url )
+    @link.ip_address = request.remote_ip if @link.new_record?
+
+    if @link.save
       respond_to do |format|
         format.xml { render :xml => @link.to_api_xml }
-        format.json { render :json => @link.to_api_json }        
+        format.json { render :json => @link.to_api_json }
       end
     else
       respond_to do |format|
         format.xml { render :xml => xml_error_response( "Unable to generate a RubyURL for you" ) }
         format.json { render :json => "Unable to generate a RubyURL for you".to_json }
-      end  
+      end
     end
   end
 end
--- spec/controllers/api/links_controller_spec.rb
+++ spec/controllers/api/links_controller_spec.rb
@@ -16,26 +16,26 @@ describe Api::LinksController, "creating a new rubyurl" do
 
   it "should create a new instance of a Link object" do
     Link.should_receive( :find_or_create_by_website_url ).with( valid_parameters[:website_url] ).and_return( @link )
-    post :create, :link => valid_parameters, :format => :xml
+    post :create, :link => valid_parameters, :format => 'xml'
   end
 
   it "should assign the remote ip address" do
     @link.should_receive( :ip_address= )
-    post :create, :link => valid_parameters, :format => :xml
+    post :create, :link => valid_parameters, :format => 'xml'
   end
 
   it "should successfully save the new link" do
     @link.should_receive( :save ).and_return( true )
-    post :create, :link => valid_parameters, :format => :xml
+    post :create, :link => valid_parameters, :format => 'xml'
   end
 
   it "should return XML" do
     @link.should_receive( :to_api_xml )
-    post :create, :link => valid_parameters, :format => :xml
+    post :create, :link => valid_parameters, :format => 'xml'
   end
 
   it "should be successful" do
-    post :create, :link => valid_parameters, :format => :xml
+    post :create, :link => valid_parameters, :format => 'xml'
     response.should be_success
   end
 
