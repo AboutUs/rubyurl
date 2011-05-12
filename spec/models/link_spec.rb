644398119f0aa65202c624ab17e725e1644bda74
Clinton R. Nixon <crnixon@gmail.com>
2011-05-11 21:21:20 -0400

    Upgrade to Rails 2.3.11

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,11 +2,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Link, "with fixtures loaded" do
   fixtures :links
-  
+
   it "should load a non-empty collection of links" do
     Link.find(:all).should_not be_empty
   end
-  
+
   it "should have four records" do
     Link.should have(4).records
   end
@@ -14,23 +14,23 @@ end
 
 describe "Planet Argon link " do
   fixtures :links, :visits
-  
+
   before(:each) do
     @link = Link.find(links(:planetargon).id)
   end
-  
+
   it "should have a matching website url" do
     @link.website_url.should eql(links(:planetargon).website_url)
   end
-  
+
   it "should have two (2) visits" do
     @link.should have(2).visits
   end
-  
+
   it "should not be flagged as spam" do
     @link.flagged_as_spam?.should be_false
   end
-  
+
   it "should add a new visit with .add_visit" do
     request = mock('request')
     request.stub!(:remote_ip).and_return('127.0.0.1')
@@ -42,53 +42,53 @@ end
 
 describe "Spammer site" do
   fixtures :links, :visits
-  
-  before(:each) do 
+
+  before(:each) do
     @link = Link.find(links(:spammer_site).id)
   end
-  
+
   it "should have one (1) visits" do
     @link.should have(1).visits
   end
-  
+
   it "should have one flagged as spam visit" do
     @link.should have(1).spam_visits
   end
-  
+
   it "should be flagged as spam" do
-    @link.flagged_as_spam?.should be_true    
+    @link.flagged_as_spam?.should be_true
   end
 end
 
 describe Link, "a new link" do
   include LinkSpecHelper
-  
+
   before(:each) do
     @link = Link.new
   end
-  
+
   it "should be invalid without a website url" do
     @link.attributes = valid_attributes.except(:website_url)
     @link.should have(1).error_on(:website_url)
   end
-  
+
   it "should be invalid without an ip address" do
     @link.attributes = valid_attributes.except(:ip_address)
-    @link.should have(1).error_on(:ip_address)    
+    @link.should have(1).error_on(:ip_address)
   end
-  
+
   it "should be valid with valid attributes" do
     @link.attributes = valid_attributes
     @link.should be_valid
   end
-  
+
   it "should generate a token when saved" do
     @link.attributes = valid_attributes
     @link.token.should be_nil
     @link.save.should be_true
     @link.token.should_not be_nil
   end
-  
+
   it "should generate a permalink when created" do
     @link.attributes = valid_attributes
     @link.permalink.should be_nil
@@ -100,13 +100,13 @@ end
 
 describe "A new Link, which already exists" do
   include LinkSpecHelper
-  
+
   before(:each) do
     @link = Link.new
     @link.attributes = valid_attributes
     @link.save
   end
-  
+
   it "should return the original link rather than create a new one" do
     new_link = Link.find_or_create_by_website_url(valid_attributes[:website_url])
     new_link.should eql(@link)
@@ -114,15 +114,15 @@ describe "A new Link, which already exists" do
 end
 
 describe "A new link" do
-  include LinkSpecHelper    
-  
+  include LinkSpecHelper
+
   it "should not save when provided a URL without http://" do
     @link = Link.new
     @link.attributes = valid_attributes.except(:website_url)
     @link.website_url = 'google.com'
     @link.should have(1).error_on(:website_url)
   end
-  
+
   it "should save a link without a .com/.net/.org/etc." do
     @link = Link.new
     @link.attributes = valid_attributes.except(:website_url)
@@ -135,7 +135,7 @@ describe "A new link" do
     @link.attributes = valid_attributes.except(:website_url)
     @link.website_url = 'http://hamsterstyle.com/foo?x=1'
     @link.should have(0).errors_on(:website_url)
-  end    
+  end
 
   it "should save a link with an achor tag and retain it" do
     @link = Link.new
@@ -151,7 +151,7 @@ describe Link, 'to_api_xml' do
     @link = Link.new( { :website_url => 'http://www.robbyonrails.com/', :ip_address => '127.0.0.1' } )
     @link.save
   end
-  
+
   it "should return the proper XML" do
     @link.to_api_xml.should == '<?xml version="1.0" encoding="UTF-8"?><link><website_url>' + @link.website_url + '</website_url><permalink>' + @link.permalink + '</permalink></link>'
   end
@@ -162,8 +162,8 @@ describe Link, 'to_api_json' do
     @link = Link.new( { :website_url => 'http://github.com/robbyrussell/', :ip_address => '127.0.0.1' } )
     @link.save
   end
-  
+
   it "should return the proper JSON" do
-    @link.to_api_json.should == '{"link": {"permalink": "' + @link.permalink + '", "website_url": "' + @link.website_url + '"}}'
+    @link.to_api_json.should == '{"link":{"permalink":"' + @link.permalink + '","website_url":"' + @link.website_url + '"}}'
   end
 end
