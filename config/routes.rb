021dafc10b8479ccb063e5731a8185ea4461efb4
Clinton R. Nixon <crnixon@gmail.com>
2011-05-12 22:09:15 -0400

    Moved routes around to get specs to pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rubyurl::Application.routes.draw do
   root :to => 'links#index'
+  resources :links
   match 'rubyurl/remote' => 'links#create'
   match 'about' => 'links#about'
   match 'api' => 'links#api'
   match 'report-abuse' => 'links#report'
   match 'home' => 'links#home'
-  resources :links
-  match '/:controller(/:action(/:id))'
   match ':token' => 'links#redirect'
+  match '/:controller(/:action)'
 end
