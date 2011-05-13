553565e0eaad9603311965c1f6767d09a23cbf86
Clinton R. Nixon <crnixon@gmail.com>
2011-05-13 17:42:43 -0400

    Changed migration to work with any DB

--- db/migrate/001_create_links.rb
+++ db/migrate/001_create_links.rb
@@ -1,15 +1,16 @@
 class CreateLinks < ActiveRecord::Migration
   def self.up
     create_table :links do |t|
-      t.column "website_url", :text
+      t.column "website_url", :string
       t.column "token",       :string
       t.column "permalink",   :string
       t.column "ip_address",  :string
       t.column "created_at",  :datetime
       t.column "updated_at",  :datetime
     end
-    
+
     add_index :links, :token
+    add_index :links, :website_url
   end
 
   def self.down
