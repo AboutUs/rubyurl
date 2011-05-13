59fff7d8990fe504269c92edeb127fc4c734cc36
Clinton R. Nixon <crnixon@gmail.com>
2011-05-12 21:52:01 -0400

    Get link specs to pass
    Not sure why this validation had :on => :save on it
    Honestly, this test should have never passed in this case

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link < ActiveRecord::Base
 
   validates_presence_of :website_url, :ip_address
   validates_uniqueness_of :website_url, :token
-  validates_format_of :website_url, :with => /^(http|https):\/\/[a-z0-9]/ix, :on => :save, :message => 'needs to have http(s):// in front of it', :if => Proc.new { |p| p.website_url? }
+  validates_format_of :website_url, :with => /^(http|https):\/\/[a-z0-9]/ix, :message => 'needs to have http(s):// in front of it', :if => Proc.new { |p| p.website_url? }
 
   before_create :generate_token
 
